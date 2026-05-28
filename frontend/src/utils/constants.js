f4a2eab3c19aa4d7208753c3b21787d0c176b3fd
quadriodebunmi <marketplacelens@gmail.com>
2026-05-29 00:32:53 +0100

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -15,6 +15,7 @@ export const CATEGORIES = [
 export const CATEGORIES_NO_ALL = CATEGORIES.slice(1);
 
 export const SORT_OPTIONS = [
+    { value: 'tiktokScore', label: 'default sort', order: null },
   { value: 'createdAt', label: 'Newest First', order: 'desc' },
   { value: 'createdAt', label: 'Oldest First', order: 'asc' },
   { value: 'rating', label: 'Highest Rated', order: 'desc' },
@@ -24,6 +25,7 @@ export const SORT_OPTIONS = [
 ];
 
 export const SELLER_SORT_OPTIONS = [
+   { value: 'tiktokScore', label: 'default sort', order: null },
   { value: 'createdAt', label: 'Newest First', order: 'desc' },
   { value: 'rating', label: 'Highest Rated', order: 'desc' },
   { value: 'rating', label: 'Lowest Rated', order: 'asc' },
