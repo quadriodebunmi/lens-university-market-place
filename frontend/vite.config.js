3a3b6bc0a8a080cb16e8d75d599298b261bf09d6
quadriodebunmi <marketplacelens@gmail.com>
2026-05-28 06:42:51 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',  // Explicitly set base path
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://lens-university-market-place-alpha.vercel.app/',
-        changeOrigin: true,
-        secure: true
-      },
-      '/uploads': {
-        target: 'https://lens-university-market-place-alpha.vercel.app/',
-        changeOrigin: true,
-        secure: true
-      }
-    }
-  }
+  
 })
